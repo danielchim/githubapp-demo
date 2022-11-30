ca64b18147f77d099ce198ff555fc79ec2c05548
danielchim <kahungchim@gmail.com>
2022-11-30 13:42:19 +0800

    inital commit

---
+++ src/redirect.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function Redirect() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+        <h1>Vite + react</h1>
+    </div>
+  )
+}
+
+export default Redirect
